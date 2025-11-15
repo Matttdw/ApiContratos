3eef7f4a308907e04e8641d79336083b2f0a6b45
Matheus Pires <mathctbapires@outlook.com>
2025-11-15 16:48:29 -0300

    app.js criado e index atualizado para carregar o JS

--- obj/Debug/net8.0/ApiContratos.AssemblyInfo.cs
+++ obj/Debug/net8.0/ApiContratos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiContratos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22322594e95ef982443c41b3ecec3947abceb667")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57375358038a55ce2526408e0bb737fafd38f8d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiContratos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiContratos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
