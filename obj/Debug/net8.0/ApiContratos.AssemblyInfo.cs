57375358038a55ce2526408e0bb737fafd38f8d5
Matheus Pires <mathctbapires@outlook.com>
2025-11-15 16:45:28 -0300

    Versão inicial do HTML sem JS e sem CSS

--- obj/Debug/net8.0/ApiContratos.AssemblyInfo.cs
+++ obj/Debug/net8.0/ApiContratos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiContratos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89efc7db4c8bdfc164c7b6d758b74a6758aaeca0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22322594e95ef982443c41b3ecec3947abceb667")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiContratos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiContratos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
